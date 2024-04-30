b0b459a7a99e423ef412d2ae204cafcc3b04e02f
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-04-30 13:47:08 -0500

    First commit

---
+++ functions/simple_functions.js
@@ -0,0 +1,32 @@
+"use strict"
+
+
+//function display my name, favorite movie, and favortie band
+function favoriteThings() {
+
+
+    console.log("My name is Adriana");
+    console.log("My favorite movie is Baby Boy");
+    console.log("My favorite band is Backstreet Boys");
+
+}
+
+function whyImHere() {
+
+    console.log("I want to learn something new and challenging");
+
+}
+
+function favoritePlace() {
+
+    console.log("My favorite place is Bali");
+
+}
+
+//calling /invoking our functions
+favoriteThings();
+whyImHere();
+favoritePlace();
+
+
+
